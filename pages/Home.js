8713ae107e97b62b3241daeabebaac48150117e6
STEPHANIE MALBERT <[STEPHANIE.MALBERT@live.fr](mailto:STEPHANIE.MALBERT@live.fr)>
2023-01-11 17:14:11 +0100

    Ajout date picker sur page Home

--- pages/Home.js
+++ pages/Home.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import DatePicker from '../components/datePicker';
 
 export default function Home() {
   return (
 <div>
     hgdcghh
+    <DatePicker/>
 </div>
   )
 }
