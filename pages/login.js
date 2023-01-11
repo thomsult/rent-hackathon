ff7e6c3f23b059dd361759ceac329f5029adb7b5
thomsult <thomsult@gmail.com>
2023-01-11 18:57:50 +0100

    update login and register

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PublicLayout from '../components/PublicLayout'
+import Link from 'next/link'
 export default function login() {
   return (
     <PublicLayout children={undefined}>
@@ -8,10 +9,10 @@ export default function login() {
         <div>
           <img
             className="h-12 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            src="/logo.png"
             alt="Workflow"
           />
-          <h2 className="mt-6 text-3xl font-extrabold text-gray-900">Login in </h2>
+          <h2 className="mt-6 text-3xl font-extrabold text-gray-900">Login</h2>
         
         </div>
 
@@ -23,7 +24,7 @@ export default function login() {
           </div>
 
           <div className="mt-6">
-            <form action="#" method="POST" className="space-y-6">
+            <form className="space-y-6">
               <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                   Email address
@@ -39,7 +40,6 @@ export default function login() {
                   />
                 </div>
               </div>
-
               <div className="space-y-1">
                 <label htmlFor="password" className="block text-sm font-medium text-gray-700">
                   Password
@@ -54,34 +54,22 @@ export default function login() {
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
                 </div>
-              </div>
-
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember-me"
-                    name="remember-me"
-                    type="checkbox"
-                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                  />
-                  <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                    Remember me
-                  </label>
-                </div>
-
-                <div className="text-sm">
-                  <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                    Forgot your password?
-                  </a>
-                </div>
-              </div>
-
+              </div>  
+              <p className="mt-8 text-sm font-medium text-gray-700">
+        Pas de Compte?{" "}
+        <Link
+          href="/register"
+          className="font-medium text-gray-500 hover:text-gray-800"
+        >
+          Enregistrement
+        </Link>
+      </p>
               <div>
                 <button
                   type="submit"
-                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  Sign in
+                  Login
                 </button>
               </div>
             </form>
--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PublicLayout from '../components/PublicLayout'
+import Link from 'next/link'
 
 
 export default function register() {
@@ -8,12 +9,12 @@ export default function register() {
         <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
       <div className="mx-auto w-full max-w-sm lg:w-96">
         <div>
-          <img
+        <img
             className="h-12 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            src="/logo.png"
             alt="Workflow"
           />
-          <h2 className="mt-6 text-3xl font-extrabold text-gray-900">Register in </h2>
+          <h2 className="mt-6 text-3xl font-extrabold text-gray-900">Register</h2>
         
         </div>
 
@@ -25,10 +26,10 @@ export default function register() {
           </div>
 
           <div className="mt-6">
-            <form action="#" method="POST" className="space-y-6">
+            <form className="space-y-6">
             <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                  Email address
+                  User Name
                 </label>
                 <div className="mt-1">
                   <input
@@ -72,28 +73,22 @@ export default function register() {
                   />
                 </div>
               </div>
+              <p className="mt-8 text-sm font-medium text-gray-700">
+        Deja un Compte?{" "}
+        <Link
+          href="/login"
+          className="font-medium text-gray-600 hover:text-gray-500"
+        >
+          Login
+        </Link>
+      </p>
 
-              <div className="space-y-1">
-                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                  Password
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
               <div>
                 <button
                   type="submit"
-                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  Sign in
+                  Register
                 </button>
               </div>
             </form>
