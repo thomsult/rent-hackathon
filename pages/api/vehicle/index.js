7ca280d5fdcc8961fb2c337d354514c656633943
thomsult <thomsult@gmail.com>
2023-01-12 00:14:47 +0100

    update vehicle add query

--- pages/api/vehicle/index.js
+++ pages/api/vehicle/index.js
@@ -2,16 +2,34 @@
 import { query } from "../../../lib/db";
 
 export default async function handler(req, res) {
+  let data = null
+  let keyList = []
+  if(req.query.where !== undefined){
+    //exemple = http://localhost:3000/api/vehicle?where&transmission=manuel&fuel_type=diesel&statut=disponible
+    delete req.query.where
+    for (const key in req.query) {
+      keyList.push(key)
+    }
+
     try {
-        const querySql = "SELECT * FROM vehicle";
-        const values = [];
-        query({ query: querySql, values: values }).then((data)=>{
-          res.status(200).json(data);
-        });
-      } catch (error) {
-        // unhide to check error
-        // res.status(500).json({ error: error.message });
-      }
-    
+      const querySql = `SELECT * FROM vehicle where ${keyList.map((el)=>`${el} = ? and `).join('')}`.slice(0, -5)
+
+      const values = [...keyList.map((el)=>req.query[el])];
+      data = await query({ query: querySql, values: values })
+    } catch (error) {
+      // unhide to check error
+       res.status(500).json({ error: error.message });
+    }
+  }else{
+    try {
+      const querySql = "SELECT * FROM vehicle";
+      const values = [];
+      data = await query({ query: querySql, values: values })
+    } catch (error) {
+      // unhide to check error
+       res.status(500).json({ error: error.message });
+    }
   }
-  
+  res.status(200).json(data);
+}
+
