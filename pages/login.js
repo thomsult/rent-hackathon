70b3bde3fd4338adb0cf5f39c19dba7616026531
thomsult <thomsult@gmail.com>
2023-01-11 20:43:33 +0100

    update home

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import PublicLayout from '../components/PublicLayout'
+import { useForm } from "react-hook-form";
+
 import Link from 'next/link'
 export default function login() {
+  const { register, handleSubmit, setValue,formState: { errors } ,watch} = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    }
+  });
+  const onSubmit = data => console.log(data);
+
   return (
     <PublicLayout children={undefined}>
     <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
@@ -24,18 +34,18 @@ export default function login() {
           </div>
 
           <div className="mt-6">
-            <form className="space-y-6">
+            <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
               <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                   Email address
                 </label>
                 <div className="mt-1">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
+                <input
+                id="email"
+                   type="email"
+                    {...register("email", { 
+                      required: true 
+                    })}
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
                 </div>
@@ -47,10 +57,11 @@ export default function login() {
                 <div className="mt-1">
                   <input
                     id="password"
-                    name="password"
                     type="password"
                     autoComplete="current-password"
-                    required
+                    {...register("password", { 
+                      required: true 
+                    })}
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
                 </div>
