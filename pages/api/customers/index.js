97e046c22a60f6a6ff513ba4ef287c86d9b95cb3
thomsult <thomsult@gmail.com>
2023-01-11 22:54:20 +0100

    update route Vehicle

--- pages/api/customers/index.js
+++ pages/api/customers/index.js
@@ -1,12 +1,12 @@
-import { query } from "../../lib/db";
+import { query } from "../../../lib/db";
 
 export default async function handler(req, res) {
   try {
     const querySql = "SELECT * FROM customers";
     const values = [];
-    const data = await query({ query: querySql, values: values });
-
-    res.status(200).json(data);
+    query({ query: querySql, values: values }).then((data)=>{
+        res.status(200).json(data);
+    });
   } catch (error) {
     // unhide to check error
     // res.status(500).json({ error: error.message });
