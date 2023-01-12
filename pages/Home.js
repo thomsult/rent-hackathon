8f35243de839323fff1a0c3103da4dca6a02e365
STEPHANIE MALBERT <[STEPHANIE.MALBERT@live.fr](mailto:STEPHANIE.MALBERT@live.fr)>
2023-01-12 15:42:38 +0100

    Travail en cours sur page Home

--- pages/Home.js
+++ pages/Home.js
@@ -31,9 +31,9 @@ axios
 <main className="h-screen flex flex-col relative">
   <nav className="bg-yellow-500 p-6 px-10 w-full">Najim</nav>
   <div className="mt-10 md:mt-0">
-    <div className="flex md:hidden w-full px-8 max-h-64 overflow-hidden">
+    <div className="flex md:hidden w-full max-h-64 overflow-hidden">
       <img
-      className="md:h-full rounded-lg object-cover object-bottom" 
+      className="bg-cover md:h-full rounded-lg object-cover object-bottom lg:" 
         src="/Home_Image.png"
         alt="Picture of the author"
       />
@@ -41,7 +41,7 @@ axios
 
     <div className="hidden md:flex max-h-[95vh] ">
       <img
-      className="w-full object-cover object-center" 
+      className="w-full object-cover object-center max-h-[33%]" 
         src="/Home_Image.png"
         alt="Picture of the author"
       />
@@ -71,3 +71,4 @@ axios
   )
 }
 // rounded-md bg-red-500 mx-10 mt-10 md:absolute bg-slate-100 p-4 top-0 md:translate-y-[40vw] lg:translate-y-[40vh]
+// caractéristiques image background: md:h-full rounded-lg object-cover object-bottom
