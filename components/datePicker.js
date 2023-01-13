c0ef9d5f34721ddcdeb9d0a4ae55e7035c22ec86
STEPHANIE MALBERT <[STEPHANIE.MALBERT@live.fr](mailto:STEPHANIE.MALBERT@live.fr)>
2023-01-13 01:07:09 +0100

    Ajout state sur composant date_picker

--- components/datePicker.js
+++ components/datePicker.js
@@ -6,13 +6,14 @@ import 'react-datepicker/dist/react-datepicker.css';
 function DateRangePicker() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
-  const [data, setData] = useState();
+  const [time, setTime] = useState('');
+  const [city, setCity] = useState('');
+  
 
   return (
     <div className="w-full text-white bg-black md:pt-4 pb-4 px-8 py-4 mt-5 lg:rounded-md flex flex-col max-w-screen items-center min-w-screen justify-between lg:flex-row">
       <div>
         <h1>Start date</h1>
-        {/* <span>from</span> */}
         <DatePicker className='rounded-md md:mt-5 mb-5 text-black'
           selected={startDate}
           onChange={date => setStartDate(date)}
@@ -22,7 +23,6 @@ function DateRangePicker() {
           placeholderText="Select start date"
         />
       </div>
-   
       <div> 
         <h1>End date</h1>
         <DatePicker className='rounded-md md:mt-5 mb-5 text-black'
@@ -38,13 +38,13 @@ function DateRangePicker() {
         <h1>
           Time 
         </h1>
-        <input className='rounded-md md:mt-5 mb-5 text-black' type="text" placeholder="Type the hour" required id="city" name="city" label="city" autoComplete="city"/>
+        <input className='rounded-md md:mt-5 mb-5 text-black' type="text" placeholder="Type the hour" onChange={(e) => setTime(e.target.value)} required id="time" name="time" label="time" autoComplete="time"/>
       </div>
       <div>
         <h1>City</h1>
-        <input className='rounded-md md:mt-5 mb-5 text-black' type="text" placeholder="Type your city" required id="city" name="city" label="city" autoComplete="city"/>
+        <input className='rounded-md md:mt-5 mb-5 text-black' type="text" placeholder="Type your city" onChange={(e) => setCity(e.target.value)} required id="city" name="city" label="city" autoComplete="city"/>
       </div>
-      <button className="2xl:bg-[#F3B33D] px-5 rounded-md max-h-[3em] xl: bg-[#F3B33D] px-5 rounded-md max-h-[3em] mt-6 bg-[#F3B33D] px-5 rounded-md max-h-[3em] md:bg-[#F3B33D] px-5 rounded-md max-h-[3em] sm: bg-[#F3B33D] px-5 py-3 rounded-md max-h-[3em]" onClick={(data) => setData}>See the cars</button>
+      <button className="2xl:bg-[#F3B33D] px-5 rounded-md max-h-[3em] xl: bg-[#F3B33D] px-5 rounded-md max-h-[3em] mt-6 bg-[#F3B33D] px-5 rounded-md max-h-[3em] md:bg-[#F3B33D] px-5 rounded-md max-h-[3em] sm: bg-[#F3B33D] px-5 py-3 rounded-md max-h-[3em]">See the cars</button>
     </div>
   );
 }
